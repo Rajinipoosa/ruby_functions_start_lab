0844b1e399171dd70b834229e3806a2ec2a10394
Rajini <rajinipoosa@gmail.com>
2017-03-07 16:26:06 +0000

    added the add function and specs to spec files

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -2,10 +2,10 @@ def return_10
   return 10
 
 end
-# def add(num1 ,num2)
-#  return num1 + num2
+def add(num1 ,num2)
+  return num1 + num2
 
-# end
+end
 
 # def subtract(num1 ,num2)
 #   return num1 - num2
