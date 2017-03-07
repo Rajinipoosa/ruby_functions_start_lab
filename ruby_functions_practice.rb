38a5c3d7c0636261856bad82693ea63b3401827f
Rajini <rajinipoosa@gmail.com>
2017-03-07 16:29:47 +0000

    Added the subract function and specs to file

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -7,10 +7,10 @@ def add(num1 ,num2)
 
 end
 
-# def subtract(num1 ,num2)
-#   return num1 - num2
+def subtract(num1 ,num2)
+   return num1 - num2
 
-# end
+ end
 
 # def multiply(num1 ,num2)
 #  return num1 * num2
