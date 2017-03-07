26fb153dbdef09c6862436e0d6daac46e3a2862b
Rajini <rajinipoosa@gmail.com>
2017-03-07 16:23:49 +0000

    functions practice and specs file has been added

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,62 @@
+def return_10
+  return 10
+
+end
+# def add(num1 ,num2)
+#  return num1 + num2
+
+# end
+
+# def subtract(num1 ,num2)
+#   return num1 - num2
+
+# end
+
+# def multiply(num1 ,num2)
+#  return num1 * num2
+
+# end
+
+# def divide(num1 ,num2)
+#  return num1 / num2
+
+# end
+
+# def length_of_string(str)
+#   return str.length
+# end
+
+#   def join_string(string_1,string_2)
+#      return string_1 + string_2
+#   end
+
+#   def add_string_as_number(string_1,string_2)
+#     return string_1.to_i+ string_2.to_i
+#   end
+
+#   def number_to_full_month_name(number)
+#     case number
+#       when 1 
+#        puts "January"
+#      when 3
+#        puts "March"
+#      when 9 
+#       puts "September"
+#      end
+#   end
+     
+#     def number_to_short_month_name(num) 
+#       case num
+#         when 1
+#           puts "Jan"
+#         when 3
+#           puts "Mar"
+#        when 9
+#         puts "Sep"
+#        end
+
+#      end
+
+  
+ 
+
